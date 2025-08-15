472bc6953638ec3a899015a4e8a65937575bd721
Bo <gold.ro@northeastern.edu>
2025-08-15 14:46:40 -0500

    Re

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./Kambaz/Dashboard.jsx";
-import Courses from "./Kambaz/Courses.jsx";
-import Assignments from "./Kambaz/Assignments.jsx";
-import Labs from "./Kambaz/Labs.jsx";
+import Dashboard from "./Kambaz/Dashboard.tsx";
+import Courses from "./Kambaz/Courses.tsx";
+import Assignments from "./Kambaz/Assignments.tsx";
+import Labs from "./Kambaz/Labs.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter basename="/">
+          <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/labs/*" element={<Labs />} />
       <Route path="/courses" element={<Courses />} />
       <Route path="/assignments" element={<Assignments />} />
+    </Routes>
     </BrowserRouter>
   );
 }
+export default App;
 
